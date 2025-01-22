e3867c5077b6018d9be5eb3adb608ce234559947
rick-dev-logex <ricardo.estrella@logex.ec>
2025-01-22 10:10:09 -0500

    Added missing traits

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Area extends Model
 {
-    //
+    use HasApiTokens;
+
+    protected $fillable = ['name', 'description'];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public function requests()
+    {
+        return $this->hasMany(Request::class);
+    }
+
+    public function transports()
+    {
+        return $this->hasMany(Transport::class);
+    }
+
+    public function accounts()
+    {
+        return $this->hasMany(Account::class);
+    }
+
+    public function personals()
+    {
+        return $this->hasMany(Personal::class);
+    }
 }
--- app/Models/Transport.php
+++ app/Models/Transport.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Transport extends Model
 {
     use HasFactory;
+    use HasApiTokens;
 
     protected $connection = 'onix'; // Conexión a onix
     protected $table = 'onix_vehiculos'; // Nombre de la tabla
+    protected $fillable = ['placa', 'marca', 'modelo', 'anio', 'tipo', 'estado', 'proyecto'];
 }

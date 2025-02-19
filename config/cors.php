b2f39cd2be27f5daf54db09dd10dc0ddcc8ebe00
rick-dev-logex <ricardo.estrella@logex.ec>
2025-02-19 11:19:30 -0500

    updated cors config

--- config/cors.php
+++ config/cors.php
@@ -1,28 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'login'],
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    // 'allowed_origins' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'https://lms.logex.com.ec',
-        'https://api.lms.logex.com.ec',
-        'https://lms-backend-898493889976.us-east1.run.app'
-    ],
+    'paths' => ['api/*', 'login', 'logout'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://lms.logex.com.ec'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => [
-        'Content-Type',
-        'Authorization',
-        'X-Requested-With',
-        'Accept',
-        'Origin',
-        'Cookie',
-        'Set-Cookie',
-        'X-CSRF-TOKEN',
-        'X-XSRF-TOKEN'
-    ],
-    'exposed_headers' => ['Authorization', 'Content-Type', 'X-Request-With'],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
 ];

bdea4865c180ec04b23b1c1205f6a4b8bf066410
rick-dev-logex <ricardo.estrella@logex.ec>
2025-02-25 17:03:00 -0500

    updated endpoint configuration with bearer token

--- app/Http/Middleware/ValidateApiToken.php
+++ app/Http/Middleware/ValidateApiToken.php
@@ -9,26 +9,20 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ValidateApiToken
 {
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        // Excluir la validación del token si la ruta es '/api/login'
-        if ($request->is('api/login')) {
+        // Verificar si el encabezado Authorization tiene el formato Bearer <token>
+        $token = $request->bearerToken();
+
+        // Token que "quemaste" o guardaste en Secrets Manager
+        $validToken = env('API_ACCESS_TOKEN');
+
+        // Validación
+        if ($token && $token === $validToken) {
             return $next($request);
         }
 
-        // Excluir la validación si el usuario ya está autenticado
-        if (Auth::check()) {
-            return $next($request);
-        }
-
-        // Obtiene el token de la URL (por ejemplo: ?authtkn=...)
-        $token = $request->query('authtkn');
-
-        // Compara con el token definido en .env
-        if (!$token || $token !== env('API_ACCESS_TOKEN')) {
-            return response()->json(['message' => 'Acceso no autorizado'], 403);
-        }
-
-        return $next($request);
+        // Si el token es inválido
+        return response()->json(['message' => 'Forbidden'], 403);
     }
 }

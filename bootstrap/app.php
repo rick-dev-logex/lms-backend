ecd6a6d7250f2482f3026dd1abb2af83be6336b8
rick-dev-logex <ricardo.estrella@logex.ec>
2025-02-26 17:50:17 -0500

    removed temp config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,9 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-if (env('CLEAR_CACHE') === 'true') {
-    \Illuminate\Support\Facades\Artisan::call('config:clear');
-}
+// if (env('CLEAR_CACHE') === 'true') {
+//     \Illuminate\Support\Facades\Artisan::call('config:clear');
+// }
 
 
 return Application::configure(basePath: dirname(__DIR__))

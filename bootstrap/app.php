e2ebade67721698837eae48280d5edeca8afe964
rick-dev-logex <ricardo.estrella@logex.ec>
2025-02-26 17:44:51 -0500

    added temp config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+if (env('CLEAR_CACHE') === 'true') {
+    \Illuminate\Support\Facades\Artisan::call('config:clear');
+}
+
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',

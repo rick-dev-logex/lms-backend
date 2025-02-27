4bf2c8b4fd0bd358dcc7e2d17e3bfbbd777d623a
rick-dev-logex <ricardo.estrella@logex.ec>
2025-02-27 12:44:32 -0500

    added explicit connection for project assignments

--- app/Models/UserAssignedProjects.php
+++ app/Models/UserAssignedProjects.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserAssignedProjects extends Model
 {
+    protected $connection = 'lms_backend';
     protected $table = 'user_assigned_projects';
 
     // Aunque el campo es LONGTEXT, lo casteamos a array para trabajar como JSON

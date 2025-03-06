6b489e7294391c72e722350774a0202982603017
rick-dev-logex <ricardo.estrella@logex.ec>
2025-03-06 16:23:02 -0500

    updated cors config

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'https://lms.logex.com.ec, http://localhost:3000, http://localhost:3001')),
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'https://lms.logex.com.ec, http://localhost:3000')),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],

436d6ff82338e973b2ec72ac86377a0a8b7a95af
rick-dev-logex <ricardo.estrella@logex.ec>
2025-03-06 12:57:41 -0500

    changed middleware priority

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         // Global middleware - se ejecuta primero 
         $middleware->use([
+            \App\Http\Middleware\HandleCors::class,
             \App\Http\Middleware\ForceHttps::class,
             \App\Http\Middleware\TrustProxies::class,
-            \App\Http\Middleware\HandleCors::class,
             \App\Http\Middleware\ValidateApiToken::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,

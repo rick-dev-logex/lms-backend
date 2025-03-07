e815b750fb84a40809f627610de0d53c7f2fac5e
rick-dev-logex <ricardo.estrella@logex.ec>
2025-03-07 11:49:50 -0500

    updated cors config file

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'https://lms.logex.com.ec, http://localhost:3000')),
+    'allowed_origins' => ['https://lms.logex.com.ec'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],

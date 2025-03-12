186cfc288a34e95839b9dc8fee057dd7417b24fc
rick-dev-logex <ricardo.estrella@logex.ec>
2025-03-12 11:33:19 -0500

    eliminar .env de git

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 
 
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('GOOGLE_CLOUD_KEY_BASE64')) {
+            $keyPath = storage_path('app/google-cloud-key.json');
+            file_put_contents($keyPath, base64_decode(env('GOOGLE_CLOUD_KEY_BASE64')));
+        }
         Schema::defaultStringLength(191);
         if ($this->app->environment('production')) {
             URL::forceScheme('https');

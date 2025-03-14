4e0284188d15bf0b844ceff66c93fa5ac49adb95
rick-dev-logex <ricardo.estrella@logex.ec>
2025-03-14 17:25:12 -0500

    updated logout method

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -65,7 +65,6 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        Log::info('Inicio de login', $request->all());
         $request->validate([
             'email'    => 'required|email',
             'password' => 'required',
@@ -124,14 +123,15 @@ class AuthController extends Controller
         }
     }
 
-
     /**
      * Cerrar sesión
      */
     public function logout(Request $request)
     {
-        $request->user()->currentAccessToken()->delete();
-        return response()->json(['message' => 'Sesión cerrada exitosamente']);
+        $cookie = Cookie::forget('jwt-token');
+        return response()->json([
+            'message' => 'Sesión cerrada exitosamente'
+        ])->withCookie($cookie);
     }
 
     /**

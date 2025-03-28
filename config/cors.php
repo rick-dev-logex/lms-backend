bbd8d8c7105e5f42f8ec06e9587895495e74eb76
rick-dev-logex <ricardo.estrella@logex.ec>
2025-03-28 11:26:36 -0500

    allow every incoming connection

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://lms.logex.com.ec'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],

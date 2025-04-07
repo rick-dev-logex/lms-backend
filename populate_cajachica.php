da8bc0c5c1b78a04bbed330f34f43a1d2f96aa20
rick-dev-logex <ricardo.estrella@logex.ec>
2025-04-07 01:26:07 -0500

    fixed script

--- populate_cajachica.php
+++ populate_cajachica.php
@@ -3,7 +3,6 @@
 use App\Models\Request;
 use App\Models\CajaChica;
 use Illuminate\Support\Str;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 require __DIR__ . '/vendor/autoload.php';
@@ -12,7 +11,7 @@ $app = require_once __DIR__ . '/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
-echo "Iniciando migración real a caja_chica...\n";
+echo "Iniciando migración a caja_chica...\n";
 
 try {
     $requests = Request::all(); // Volvemos a todos los registros
@@ -51,20 +50,20 @@ try {
         CajaChica::updateOrCreate(
             ['id' => $request->id],
             [
-                'fecha'             => $request->request_date,
-                'codigo'            => $codigo,
-                'descripcion'       => $request->note,
-                'saldo'             => $request->amount,
-                'centro_costo'      => $centroCosto,
-                'cuenta'            => $numeroCuenta,
-                'nombre_de_cuenta'  => $nombreCuenta,
-                'proveedor'         => $proveedor,
-                'empresa'           => 'SERSUPPORT',
-                'proyecto'          => $proyecto,
-                'i_e'               => 'EGRESO',
-                'mes_servicio'      => $mesServicio, // Ahora es '2025-04-16' en lugar de '16/4/2025'
-                'tipo'              => $tipo,
-                'estado'            => $request->status,
+                'FECHA'             => $request->request_date,
+                'CODIGO'            => $codigo,
+                'DESCRIPCION'       => $request->note,
+                'SALDO'             => $request->amount,
+                'CENTRO_COSTO'      => $centroCosto,
+                'CUENTA'            => $numeroCuenta,
+                'NOMBRE_DE_CUENTA'  => $nombreCuenta,
+                'PROVEEDOR'         => $proveedor,
+                'EMPRESA'           => 'SERSUPPORT',
+                'PROYECTO'          => $proyecto,
+                'I_E'               => 'EGRESO',
+                'MES_SERVICIO'      => $mesServicio, // Ahora es '2025-04-16' en lugar de '16/4/2025'
+                'TIPO'              => $tipo,
+                'ESTADO'            => $request->status,
                 'created_at'        => now(),
                 'updated_at'        => now(),
             ]
@@ -75,5 +74,5 @@ try {
     echo "✅ Migración completada correctamente.\n";
 } catch (\Exception $e) {
     DB::rollBack();
-    echo "❌ Ocurrió un error: " . $e->getMessage() . "\n";
+    echo "Ocurrió un error: " . $e->getMessage() . "\n";
 }
